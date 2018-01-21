8fcca206b891068d79f2d71faeb6a15695ee961f
requestFocus <thezodiac@wp.pl>
2018-01-21 19:39:50 +0100

    test

--- Flappy Bird Game/Assets/Scripts/PlayerProfileController.cs
+++ Flappy Bird Game/Assets/Scripts/PlayerProfileController.cs
@@ -28,7 +28,7 @@ public class PlayerProfileController : MonoBehaviour {
 
 	public bool CheckIfProfileExist(string playerName)
 	{
-		Debug.Log("CheckIfProfileExist dla " + playerName);
+		Debug.Log("CheckIfProfileExist dla sdfsdf" + playerName);
 		return PlayerPrefs.GetString("ProfileSettings").Contains(playerName);
 	}
 }
