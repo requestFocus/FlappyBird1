66598fe174b6aab144309cd0fe9e0c532c03c214
requestFocus <thezodiac@wp.pl>
2018-01-23 21:23:19 +0100

    poczatek prac nad Game Scene
    + player colliders
    + background colliders

---
+++ Flappy Bird Game/Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D player;
+	private float speed;
+
+	void Start () {
+		player = GetComponent<Rigidbody2D>();
+		speed = 5;
+	}
+
+	private void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		player.AddForce(movement * speed);
+	}
+}
