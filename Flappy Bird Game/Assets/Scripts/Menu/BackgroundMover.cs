4b4d91c167ee835d6e302e4e23be92b1afd44b56
requestFocus <thezodiac@wp.pl>
2018-02-04 15:57:23 +0100

    animacja tła-nieba

--- Flappy Bird Game/Assets/Scripts/Menu/BackgroundMover.cs
+++ Flappy Bird Game/Assets/Scripts/Menu/BackgroundMover.cs
@@ -6,26 +6,33 @@ public class BackgroundMover : MonoBehaviour {
 
 	private float _horizontalMove;
 	private float _speed;
+	private float _rightEdge1;
+	private float _rightEdge2;
 
 	private void Start()
 	{
-		_horizontalMove = -0.024f;
+		_horizontalMove = 0.05f;
+		_speed = 5.0f;
+		_rightEdge1 = -11.723f;
+		_rightEdge2 = 6.68f;
 	}
 
-	void Update ()
+	void FixedUpdate ()
 	{
 		MoveBackground();
 	}
 
 	private void MoveBackground()
 	{
-		if (transform.position.x >= -0.024f && transform.position.x <= 23.42f)
+		if (transform.position.x >= _rightEdge1 && transform.position.x < _rightEdge2)
 		{
-			transform.position = new Vector2(_horizontalMove, 0.0f);
-			_horizontalMove = _horizontalMove + 0.004f;
+			//_horizontalMove = _horizontalMove + 0.004f;
+			transform.position += (new Vector3(_horizontalMove, 0.0f, 0.0f) * Time.deltaTime * _speed);
+		}
+		else
+		{
+			transform.position  = new Vector2(_rightEdge1, 0.0f);
 		}
 	}
 }
 
-
-// -0.024 do 23.42
