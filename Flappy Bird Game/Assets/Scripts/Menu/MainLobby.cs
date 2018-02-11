710d0cf723f6ec0dcb9b558b794546f322f0e4b0
requestFocus <thezodiac@wp.pl>
2018-02-11 13:51:18 +0100

    dodane naiwne przewijanie achievementow
    event wywoluje się dwa razy na klatkę w OnGUI

--- Flappy Bird Game/Assets/Scripts/Menu/MainLobby.cs
+++ Flappy Bird Game/Assets/Scripts/Menu/MainLobby.cs
@@ -28,6 +28,9 @@ public class MainLobby : MonoBehaviour {
 	public Texture HowtoPlayButtonInactive;
 	public Texture NewGameButtonInactive;
 
+	public Texture NextAchievementPage;
+	public Texture PreviousAchievementPage;
+
 	public PlayerProfile PlayerProfile;
 	public PlayerProfileController PlayerProfileController;				
 	public ResizeController ResizeController;
@@ -48,8 +51,9 @@ public class MainLobby : MonoBehaviour {
 
 	private Rect _nextAchievementPage;
 	private Rect _previousAchievementPage;
-	private int _listAchievementfFrom;
-	private int _achievementPageScope;
+	private float _listAchievementsFrom;
+	private float _listAchievementsTo;
+	private float _scope;
 
 	private Vector2 _myMousePosition;
 	private string _justPlayerName;
@@ -66,11 +70,13 @@ public class MainLobby : MonoBehaviour {
 		NewGame,
 		GamePlay
 	};
-	MenuScreens MenuStates; // = MenuScreens.MainMenu;		
+	MenuScreens MenuStates;
+
+
 
 	private void Start()
 	{
-		PlayerPrefs.DeleteAll();
+		//PlayerPrefs.DeleteAll();
 
 		SetGUIStyle();
 	
@@ -79,18 +85,15 @@ public class MainLobby : MonoBehaviour {
 		_justPlayerName = "";
 		_isThereAList = PlayerProfileController.LoadProfiles();                             // jeśli lista istnieje, jej zawartość od razu wchodzi do singletona
 
-		_listAchievementfFrom = 0;
-		_achievementPageScope = 2;
+		_scope = 5;
+		_listAchievementsFrom = 0;
+		_listAchievementsTo = _scope;
 	}
 
-	private void Update()
-	{
-		_myMousePosition = Input.mousePosition;
-	}
+
 
 	private void OnGUI()
 	{
-
 		switch (MenuStates)
 		{
 			case MenuScreens.MainMenu:
@@ -114,6 +117,8 @@ public class MainLobby : MonoBehaviour {
 		}
 	}	
 
+
+
 	private void SetGUIStyle()
 	{
 		_labelStyle = new GUIStyle
@@ -132,6 +137,8 @@ public class MainLobby : MonoBehaviour {
 		_lightGreyFont = "3f6a84";                                     // dark grey
 	}
 
+
+
 	private Rect DrawElement(int x, int y, int width, int height, Texture menuElement, ResizeController.Horizontal horizontalAlignment, ResizeController.Vertical verticalAlignment)
 	{
 		Rect RectScalableDimensions = ResizeController.ResizeGUI(new Rect(x, y, width, height), horizontalAlignment, verticalAlignment);
@@ -139,6 +146,8 @@ public class MainLobby : MonoBehaviour {
 
 		return RectScalableDimensions;
 	}
+
+
 
 	private void DrawMainMenu()								// obsluga MAIN MENU
 	{
@@ -162,8 +171,8 @@ public class MainLobby : MonoBehaviour {
 		//DrawElement(375, 275, 50, 50, CreditsButton, "right", "bottom");
 		//====================================================================================================================================
 
-		//_myMousePosition = Event.current.mousePosition;  // Event.current.mousePosition operuje w przestrzeni top left to bottom right	
-		_myMousePosition = Input.mousePosition;
+		_myMousePosition = Event.current.mousePosition;  // Event.current.mousePosition operuje w przestrzeni top left to bottom right	
+		 //_myMousePosition = Input.mousePosition;
 
 		if (Input.GetMouseButtonDown(0))
 		{
@@ -201,6 +210,8 @@ public class MainLobby : MonoBehaviour {
 		}
 	}
 
+
+
 	private void DrawNewGameMenu(Texture menuElement)               // obsluga NEW GAME
 	{
 		_justPlayerName = GUI.TextField(ResizeController.ResizeGUI(new Rect(350, 270, 100, 25), ResizeController.Horizontal.center, ResizeController.Vertical.center), _justPlayerName, 10);
@@ -211,8 +222,8 @@ public class MainLobby : MonoBehaviour {
 
 		GUI.Label(ResizeController.ResizeGUI(new Rect(350, 360, 100, 25), ResizeController.Horizontal.center, ResizeController.Vertical.center), _labelContent, _labelStyle);
 
-		//_myMousePosition = Event.current.mousePosition;  // Event.current.mousePosition operuje w przestrzeni top left to bottom right	
-		_myMousePosition = Input.mousePosition;  
+		_myMousePosition = Event.current.mousePosition;  // Event.current.mousePosition operuje w przestrzeni top left to bottom right	
+		 //_myMousePosition = Input.mousePosition;  
 
 		if (Input.GetMouseButtonDown(0))
 		{
@@ -236,10 +247,14 @@ public class MainLobby : MonoBehaviour {
 		}
 	}
 
+
+
 	private void DrawGamePlay()										// GRA
 	{
 		SceneManager.LoadScene("Game");
 	}
+
+
 
 	private void DrawCreditsMenu(Texture menuElement)               // obsluga CREDITS
 	{ 
@@ -251,8 +266,8 @@ public class MainLobby : MonoBehaviour {
 								"SPECIAL THANKS TO\n<color=#" + _lightGreyFont + ">MICHAŁ PODYMA</color></color>";
 		GUI.Label(ResizeController.ResizeGUI(new Rect(300, 300, 200, 30), ResizeController.Horizontal.center, ResizeController.Vertical.center), _labelContent, _labelStyle);
 
-		//_myMousePosition = Event.current.mousePosition;  // Event.current.mousePosition operuje w przestrzeni top left to bottom right	
-		_myMousePosition = Input.mousePosition;
+		_myMousePosition = Event.current.mousePosition;  // Event.current.mousePosition operuje w przestrzeni top left to bottom right	
+		 //_myMousePosition = Input.mousePosition;
 
 		if (Input.GetMouseButtonDown(0))
 		{
@@ -263,26 +278,38 @@ public class MainLobby : MonoBehaviour {
 			}
 		}
 	}
+
+
 
 	private void DrawAchievementsMenu(Texture menuElement)
 	{
 		_logoRect = DrawElement(315, 20, 170, 170, LogoButton, ResizeController.Horizontal.center, ResizeController.Vertical.top);
 		DrawElement(350, 550, 100, 30, menuElement, ResizeController.Horizontal.center, ResizeController.Vertical.bottom);
 
-		ListAchievements();
+		if (_isThereAList)       // jesli istnieje lista w pamieci
+		{
+			ListAchievements(_listAchievementsFrom, _listAchievementsTo);
+		}
+		else                                                                                            // jesli w pamieci nie istnieje lista userów
+		{
+			GUI.Label(ResizeController.ResizeGUI(new Rect(300, 300, 200, 30), ResizeController.Horizontal.center, ResizeController.Vertical.center), "<color=#" + _darkGreyFont + ">No results yet.</color>", _labelStyle);
+		}
 
-		//_myMousePosition = Event.current.mousePosition;  // Event.current.mousePosition operuje w przestrzeni top left to bottom right	
-		//_myMousePosition = Input.mousePosition;
-
+		_myMousePosition = Event.current.mousePosition;  // Event.current.mousePosition operuje w przestrzeni top left to bottom right	
 		if (Input.GetMouseButtonDown(0))
 		{
 			if (ClickedWithin(_logoRect))
 			{
 				MenuStates = MenuScreens.MainMenu;
 				_labelContent.text = "";
+
+				_listAchievementsFrom = 0;
+				_listAchievementsTo = _scope;
 			}
 		}
 	}
+
+
 
 	private void DrawHowtoPlayMenu(Texture menuElement) 
 	{
@@ -293,8 +320,8 @@ public class MainLobby : MonoBehaviour {
 						"BEAT HIGHSCORES, UNLOCK ACHIEVEMENTS \nAND HAVE FUN! \n\nGOOD LUCK!</color>";
 		GUI.Label(ResizeController.ResizeGUI(new Rect(300, 300, 200, 30), ResizeController.Horizontal.center, ResizeController.Vertical.center), _labelContent, _labelStyle);
 
-		//_myMousePosition = Event.current.mousePosition;  // Event.current.mousePosition operuje w przestrzeni top left to bottom right	
-		_myMousePosition = Input.mousePosition;
+		_myMousePosition = Event.current.mousePosition;  // Event.current.mousePosition operuje w przestrzeni top left to bottom right	
+		 //_myMousePosition = Input.mousePosition;
 
 		if (Input.GetMouseButtonDown(0))
 		{
@@ -305,6 +332,8 @@ public class MainLobby : MonoBehaviour {
 			}
 		}
 	}
+
+
 
 	private void CheckPlayerPrefs()															// ładowane po kliknieciu buttona START w menu NEW GAME
 	{
@@ -334,6 +363,8 @@ public class MainLobby : MonoBehaviour {
 		}
 	}
 
+
+
 	private void AddNewProfile()
 	{
 		PlayerProfile = new PlayerProfile(_justPlayerName, 0, false, false, false);                   // tworzę nowy profil gracza z domyślnymi wartościami
@@ -352,42 +383,22 @@ public class MainLobby : MonoBehaviour {
 		PlayerProfileController.SaveProfile(PlayersProfiles.Instance);                  // zapisuję dane w singletonie				czy częsty zapis do PlayerPrefs wpływa na wydajność?==================
 	}
 
-	private void ListAchievements()                                                         // ładowane po kliknieciu buttona START w menu NEW GAME
+
+
+	private void ListAchievements(float listFrom, float listTo)                                                         // ładowane po kliknieciu buttona START w menu NEW GAME
 	{
 
-		if (_isThereAList)													// jesli istnieje lista w pamieci
-		{
-			GUI.Label(ResizeController.ResizeGUI(new Rect(200, 240, 150, 30), ResizeController.Horizontal.left, ResizeController.Vertical.center), "<color=#" + _darkGreyFont + ">NAME</color>", _labelStyle);
-			GUI.Label(ResizeController.ResizeGUI(new Rect(300, 240, 150, 30), ResizeController.Horizontal.center, ResizeController.Vertical.center), "<color=#" + _darkGreyFont + ">HIGHSCORE</color>", _labelStyle);
-			GUI.Label(ResizeController.ResizeGUI(new Rect(430, 240, 150, 30), ResizeController.Horizontal.right, ResizeController.Vertical.center), "<color=#" + _darkGreyFont + ">ACHIEVEMENTS</color>", _labelStyle);
+		GUI.Label(ResizeController.ResizeGUI(new Rect(200, 240, 150, 30), ResizeController.Horizontal.left, ResizeController.Vertical.center), "<color=#" + _darkGreyFont + ">NAME</color>", _labelStyle);
+		GUI.Label(ResizeController.ResizeGUI(new Rect(300, 240, 150, 30), ResizeController.Horizontal.center, ResizeController.Vertical.center), "<color=#" + _darkGreyFont + ">HIGHSCORE</color>", _labelStyle);
+		GUI.Label(ResizeController.ResizeGUI(new Rect(430, 240, 150, 30), ResizeController.Horizontal.right, ResizeController.Vertical.center), "<color=#" + _darkGreyFont + ">ACHIEVEMENTS</color>", _labelStyle);
 
-			_nextAchievementPage = DrawElement(315, 400, 20, 20, LogoButton, ResizeController.Horizontal.center, ResizeController.Vertical.top);
-			_previousAchievementPage = DrawElement(375, 400, 20, 20, LogoButton, ResizeController.Horizontal.center, ResizeController.Vertical.top);
+		_previousAchievementPage = DrawElement(358, 400, 32, 36, PreviousAchievementPage, ResizeController.Horizontal.center, ResizeController.Vertical.top);
+		_nextAchievementPage = DrawElement(410, 400, 32, 36, NextAchievementPage, ResizeController.Horizontal.center, ResizeController.Vertical.top);
 
-			int yPosition = 270;
-			int xPosition = 465;
+		int yPosition = 270;
+		int xPosition = 465;
 
-			if (Input.GetMouseButtonDown(0))
-			{
-				if (ClickedWithin(_nextAchievementPage))
-				{
-					//_listAchievementfFrom += 3;
-					//_achievementPageScope += 3;
-					//_achievementPageScope = (_achievementPageScope > PlayersProfiles.Instance.ListOfProfiles.Count) ? _achievementPageScope : PlayersProfiles.Instance.ListOfProfiles.Count;
-					Debug.Log("clicked within next page");
-				}
-
-				else if (ClickedWithin(_previousAchievementPage))
-				{
-					//	_listAchievementfFrom -= 3;
-					//	_achievementPageScope += 3;
-					//	//_achievementPageScope = (_achievementPageScope < 3) ? _achievementPageScope : 3;
-					Debug.Log("clicked within previous page");
-				}
-			}
-
-			//for (int i = 0; i < PlayersProfiles.Instance.ListOfProfiles.Count; i++)
-			for (int i = _listAchievementfFrom; i < _achievementPageScope; i++)
+			for (int i = (int)listFrom; i < PlayersProfiles.Instance.ListOfProfiles.Count && i < (int)listTo; i++)								// wypisze liste userów od A do B
 			{
 				// wypisz wyniki
 				GUI.Label(ResizeController.ResizeGUI(new Rect(200, yPosition, 150, 30), ResizeController.Horizontal.center, ResizeController.Vertical.center),
@@ -398,7 +409,7 @@ public class MainLobby : MonoBehaviour {
 
 				if (PlayersProfiles.Instance.ListOfProfiles[i].Complete10)
 				{
-					DrawElement(xPosition, yPosition, 23, 28, Complete10Active, ResizeController.Horizontal.center, ResizeController.Vertical.top);			// IKONY ACHIEVEMENTOW MAJA WYMIARY 96x110
+					DrawElement(xPosition, yPosition, 23, 28, Complete10Active, ResizeController.Horizontal.center, ResizeController.Vertical.top);         // IKONY ACHIEVEMENTOW MAJA WYMIARY 96x110
 				}
 				else
 				{
@@ -428,17 +439,37 @@ public class MainLobby : MonoBehaviour {
 				yPosition += 30;
 				xPosition = 465;
 			}
-		}
-		else                                                                                            // jesli w pamieci nie istnieje lista userów
-		{
-			// nie ma listy, nie ma wyników
-			GUI.Label(ResizeController.ResizeGUI(new Rect(300, 300, 200, 30), ResizeController.Horizontal.center, ResizeController.Vertical.center), "<color=#" + _darkGreyFont + ">No results yet.</color>", _labelStyle);
-		}
+
+			if (Input.GetMouseButtonDown(0))
+			{
+				if (ClickedWithin(_nextAchievementPage) && _listAchievementsTo < PlayersProfiles.Instance.ListOfProfiles.Count)
+				//if (ClickedWithin(_nextAchievementPage))
+				{
+					_listAchievementsFrom += (_scope / 2);
+					_listAchievementsTo += (_scope / 2);
+				}
+				else if (ClickedWithin(_nextAchievementPage) && _listAchievementsTo %2 != 0 && (_listAchievementsFrom + (_scope /2)) < PlayersProfiles.Instance.ListOfProfiles.Count)
+				{
+					_listAchievementsFrom += (_scope / 2);
+					_listAchievementsTo += (_scope / 2);
+				}
+
+				if (ClickedWithin(_previousAchievementPage) && _listAchievementsFrom > 0)
+					{
+						_listAchievementsFrom -= (_scope / 2);
+						_listAchievementsTo -= (_scope / 2);
+					}
+
+				Debug.Log(_listAchievementsFrom + " :: " + _listAchievementsTo);
+			}
+		MenuStates = MenuScreens.Achievements;
 	}
+
+
 
 	private bool ClickedWithin(Rect rect)
 	{
-		return ((_myMousePosition.x >= rect.x) && (_myMousePosition.x <= (rect.x + rect.width)) && ((Screen.height - _myMousePosition.y) >= rect.y) && ((Screen.height - _myMousePosition.y) <= (rect.y + rect.height)));
+		return ((_myMousePosition.x >= rect.x) && (_myMousePosition.x <= (rect.x + rect.width)) && (_myMousePosition.y >= rect.y) && (_myMousePosition.y <= (rect.y + rect.height)));
 	}
 }
 
