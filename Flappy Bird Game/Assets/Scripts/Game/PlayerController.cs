614211b2407139876cce4843dae3af6b177b1b7d
requestFocus <thezodiac@wp.pl>
2018-02-14 21:17:35 +0100

    cd grawitacji

--- Flappy Bird Game/Assets/Scripts/Game/PlayerController.cs
+++ Flappy Bird Game/Assets/Scripts/Game/PlayerController.cs
@@ -1,14 +1,103 @@
-﻿using System.Collections;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+//public class PlayerController : MonoBehaviour {
+
+//	private Rigidbody2D _player;
+//	private float _speed;
+
+//	public PlayerProfileController PlayerProfileController;
+//	public GameManager GameManager;
+
+//	public ParticleSystem PlayerParticles;
+
+//	public float Sensitivity;
+//	public float Velocity;
+//	public float Gravity;
+//	public float Range;
+
+//	public Vector3 OldPosition;
+
+//	void Start()
+//	{
+//		_player = GetComponent<Rigidbody2D>();
+
+//		Sensitivity = 1.0f;
+//		Velocity = 0.0f;
+//		Gravity = 0.2f;
+//		Range = 2.0f;
+//	}
+
+//	private void Update()
+//	{
+//		MoveBee();
+//	}
+
+//	//private void OnCollisionEnter2D(Collision2D _collision)
+//	//{
+//	//	if (_collision.gameObject.CompareTag("Obstacle"))
+//	//	{
+//	//		GameManager.CanvasController.DisplayCanvasOnSummary();
+//	//	}
+//	//}
+
+//	private void OnTriggerEnter2D(Collider2D collision)
+//	{
+//		if (collision.gameObject.CompareTag("Score"))														// zdobyty punkt
+//		{
+//			GameManager.SetScore();
+//			if (GameManager.AchievementToUnlock())
+//			{
+//				Debug.Log("achievement unlocked");
+//				PlayerParticles.Play();
+//			}
+//		}
+
+//		if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Obstacle"))		// stracone życie
+//		{
+//			GameManager.CanvasController.DisplayCanvasOnSummary();
+//		}
+//	}
+
+//	private void MoveBee()
+//	{
+//		float moveVertical = Input.GetAxis("Vertical");
+//		Vector2 movement = new Vector2(-5.0f, moveVertical * Sensitivity * Range);
+
+//		if (transform.position.y >= OldPosition.y)                                          // jesli player jest aktualnie nad swoją poprzednią pozycją to znaczy, że się wzniosl
+//		{
+//			Debug.Log("============================ UP // RESET ACCELERATION ==========================");
+//			Velocity = 0.0f;                                                                // zatem przyspieszenie jest zerowane
+//		}
+//		Velocity += Gravity * Time.deltaTime;                                               // przyspieszenie to iloczyn grawitacji i czasu
+//		movement.y -= Velocity;                                                             // obniz playera wzgledem osi y na bazie wartosci przyspieszenia
+
+//		OldPosition.y = transform.position.y;                                               // uaktualnij zawartosc poprzedniej pozycji
+//		transform.position = movement;                                                      // przemiesc playera o wyliczoną wartosc
+
+//		Debug.Log("transform.position.y: " + transform.position.y + " Velocity: " + Velocity + " // moveVertical: " + moveVertical + " // Time.deltaTime: " + Time.deltaTime);
+//	}
+//}
+
+
+
+//// gravity scale in inspector = 0.2
+//// instantiate'owanie prefabów Branch wyłączone 
+////_player.AddForce(_movement * _speed);
+
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour {
-
-	private Rigidbody2D _player;
-	private float _speed;
-
-	public PlayerProfileController PlayerProfileController;
+public class PlayerController : MonoBehaviour
+{
+	//private Rigidbody2D _player;
+	//public PlayerProfileController PlayerProfileController;
 	public GameManager GameManager;
 
 	public ParticleSystem PlayerParticles;
@@ -17,17 +106,23 @@ public class PlayerController : MonoBehaviour {
 	public float Velocity;
 	public float Gravity;
 	public float Range;
+	public float MoveVertical;
 
-	public Vector3 OldPosition;
+	public Vector2 OldPosition;
+	public Vector2 GravityMovement;
+	public Vector2 BeeMovement;
 
 	void Start()
 	{
-		_player = GetComponent<Rigidbody2D>();
+		//_player = GetComponent<Rigidbody2D>();
 
-		Sensitivity = 1.0f;
+		Sensitivity = 1f;
 		Velocity = 0.0f;
-		Gravity = 0.2f;
-		Range = 1.0f;
+		Gravity = 0.1f;
+		Range = 5.0f;
+
+		GravityMovement = new Vector2(0.0f, 0.0f);
+		BeeMovement = new Vector2(-5.0f, 0.0f);
 	}
 
 	private void Update()
@@ -35,17 +130,9 @@ public class PlayerController : MonoBehaviour {
 		MoveBee();
 	}
 
-	//private void OnCollisionEnter2D(Collision2D _collision)
-	//{
-	//	if (_collision.gameObject.CompareTag("Obstacle"))
-	//	{
-	//		GameManager.CanvasController.DisplayCanvasOnSummary();
-	//	}
-	//}
-
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		if (collision.gameObject.CompareTag("Score"))														// zdobyty punkt
+		if (collision.gameObject.CompareTag("Score"))                                                       // zdobyty punkt
 		{
 			GameManager.SetScore();
 			if (GameManager.AchievementToUnlock())
@@ -55,7 +142,7 @@ public class PlayerController : MonoBehaviour {
 			}
 		}
 
-		if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Obstacle"))		// stracone życie
+		if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Obstacle"))     // stracone życie
 		{
 			GameManager.CanvasController.DisplayCanvasOnSummary();
 		}
@@ -63,25 +150,24 @@ public class PlayerController : MonoBehaviour {
 
 	private void MoveBee()
 	{
-		float moveVertical = Input.GetAxis("Vertical");
-		Vector2 movement = new Vector2(-5.0f, moveVertical * Sensitivity * Range);
+		MoveVertical = Input.GetAxis("Vertical");
 
-		if (transform.position.y >= OldPosition.y)                                          // jesli player jest aktualnie nad swoją poprzednią pozycją to znaczy, że się wzniosl
+		if (transform.position.y > OldPosition.y)												 // jesli player jest aktualnie nad swoją poprzednią pozycją to znaczy, że się wzniosl
 		{
-			Debug.Log("============================ UP // RESET ACCELERATION ==========================");
-			Velocity = 0.0f;                                                                // zatem przyspieszenie jest zerowane
-			
+			Debug.Log("============================ UP => RESET ACCELERATION ==========================");
+			//Velocity = 0.0f;                                                           
+			GravityMovement.y = 0.0f;                                                           // zatem przyspieszenie (a dokładniej - polozenie playera wzgledem osi y na bazie wartosci przyspieszenia) jest zerowane
 		}
-		Velocity += Gravity * Time.deltaTime;                                               // przyspieszenie to iloczyn grawitacji i czasu
 
-		movement.y -= Velocity;                                                             // obniz playera wzgledem osi y na bazie wartosci przyspieszenia
+		Velocity += Gravity * Time.deltaTime;													// przyspieszenie to iloczyn grawitacji i czasu trwania klatki
+		GravityMovement.y -= Velocity;                                                             // wylicz polozenie playera wzgledem osi y na bazie wartosci przyspieszenia
 
-		OldPosition.y = transform.position.y;                                               // uaktualnij zawartosc poprzedniej pozycji
-		transform.position = movement;                                                      // przemiesc playera o wyliczoną wartosc
+		BeeMovement.y = MoveVertical * Range;
+			
+		OldPosition.y = transform.position.y;													 // uaktualnij zawartosc poprzedniej pozycji
 
-		//string biggerthan = transform.position.y > OldPosition.y ? "wznosze sie" : "opadam";
-		//Debug.Log("OldPosition.y: " + OldPosition.y + " // transform.position.y: " + transform.position.y + " // Velocity: " + Velocity);
-		Debug.Log("transform.position.y: " + transform.position.y + " Velocity: " + Velocity + " // moveVertical: " + moveVertical + " // Time.deltaTime: " + Time.deltaTime);
+		transform.position = BeeMovement + GravityMovement;                                     // przemiesc playera o wyliczoną wartosc
+		Debug.Log("odczyt z klawiatury: " + MoveVertical + " // BeeMovement.y: " + BeeMovement.y + " + GravityMovement.y: " + GravityMovement.y + " = finalna pozycja Y: " + transform.position.y);
 	}
 }
 
