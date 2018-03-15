8c058ead5fe287f58e888ec6266dcce566b65b3f
thezodiac@wp.pl <01github997$>
2018-03-15 14:19:29 +0100

    SetModel() jako funkcja uogólniona

--- Flappy Bird Game/Assets/Scripts/Menu/View.cs
+++ Flappy Bird Game/Assets/Scripts/Menu/View.cs
@@ -2,27 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
-public class View : MonoBehaviour {
-
-	private MainLobbyModel _mainLobbyModel;
-	private ProfileModel _profileModel;
-	private AchievementsModel _achievementsModel;
-
-	public void SetModel(MainLobbyModel model)
-	{
-		_mainLobbyModel = model; 
-	}
-
-
-
-	public void SetModel(ProfileModel model)
-	{
-		_profileModel = model;
-	}
-=======
 public class View<T> : MonoBehaviour {
->>>>>>> Generics
+
 
 	/* 
 	 * Widoki, które nie posiadają Modelu nie dziedziczą po View<T>, tylko bezpośrednio po MonoBehaviour
