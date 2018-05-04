95c630d7cd1b7904701c770c890926aaa5521cfc
requestFocus <thezodiac@wp.pl>
2018-05-04 16:54:31 +0200

    opis ponizej
    1) Instances powstałe z prefabów są zmiennymi lokalnymi poszczególnych funkcji fabryki
    
    2) SummaryModel nie trzyma już referencji do singletona ;)
    
    3) Fabryka stała się samodzielnym bytem, tworzone przez nią produkty są niezależne od siebie
    
    4) klasa GUIMain wyleciała, jej rolę przejął ViewManager, inicjalizacja CurrentGameStateService.CurrentGameState na stan GamePlay i pierwsze wywołanie SwitchView ma miejsce w ViewManagerze
    
    5) ViewManager dziedziczy po Singleton - tutaj ta ostatnia już jako klasa generyczna
    Tutaj mam problem, nie potrafię sobie poradzić ze stworzeniem instancji klasy TFactory w getterze singletona. Klasa ViewFactory musi dziedziczyć po MonoBehaaviour z powodu działań, które wykonuje zatem odpada rozwiązanie j/w (czyli GameObject -> AddComponent()), nie można też użyć Instantiate, bo operuję na typie, a nie na instancji. Jedyne rozwiązanie jako przyszło mi do głowy to użycie Activator.CreateInstance(), które działać działa poprawnie, ale zgłaszany jest warning i nie tworzy się GameObject na scenie ("You are trying to create a MonoBehaviour using the 'new' keyword").
    W jaki sposób stworzyć instancję klasy uogólnionej w getterze singletona, kiedy wiem, że dziedziczy po MonoBehaaviour i nie znam docelowego typu?

---
+++ Flappy Bird Game/Assets/Scripts/Game/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<TFactory> : MonoBehaviour
+{
+	private static TFactory _factoryInstance;
+
+	public static TFactory FactoryInstance
+	{
+		get
+		{
+			if (_factoryInstance == null)
+			{
+				_factoryInstance = (TFactory)Activator.CreateInstance(typeof(TFactory));        // WARNING: createinstance wykonane, ale "You are trying to create a MonoBehaviour using the 'new' keyword"
+			}
+			return _factoryInstance;
+		}
+	}
+}
--- Flappy Bird Game/Assets/Scripts/Game/ViewManager.cs
+++ Flappy Bird Game/Assets/Scripts/Game/ViewManager.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ViewManager : SingletonViewFactory                                                                   // działa, jeśli ViewManager dziedziczy z SingletonViewFactory
+public class ViewManager : Singleton<ViewFactory>                                                                  
 {
 	private GUIGamePlayView GUIGamePlayView;
 	private ISummaryView SummaryView;
 
 	private void Awake()
 	{
-		CurrentGameStateService.CurrentGameState = CurrentGameStateService.GameStates.GamePlay;		// POCZĄTEK SCENY
+		CurrentGameStateService.CurrentGameState = CurrentGameStateService.GameStates.GamePlay;     // POCZĄTEK SCENY
 		SwitchView();                                                                               // ZAŁADOWANIE WIDOKU
 	}
 
@@ -20,17 +20,17 @@ public class ViewManager : SingletonViewFactory                                 
 		switch (CurrentGameStateService.CurrentGameState)
 		{
 			case CurrentGameStateService.GameStates.GamePlay:
-				GUIGamePlayView = SingletonViewFactoryInstance.ConcreteGUIGamePlayView();                           // działa, jeśli ViewManager dziedziczy z SingletonViewFactory
+				GUIGamePlayView = FactoryInstance.ConcreteGUIGamePlayView();                                        
 				GUIGamePlayView.transform.SetParent(FindObjectOfType<ViewManager>().transform);
 				break;
 
 			case CurrentGameStateService.GameStates.SummarySuccess:
-				SummaryView = SingletonViewFactoryInstance.ConcreteGUISuccessSummaryView(GUIGamePlayView);        // działa, jeśli ViewManager dziedziczy z SingletonViewFactory
+				SummaryView = FactoryInstance.ConcreteGUISuccessSummaryView(GUIGamePlayView);                      
 				((GUISuccessSummaryView)SummaryView).transform.SetParent(FindObjectOfType<ViewManager>().transform);
 				break;
 
 			case CurrentGameStateService.GameStates.SummaryFailure:
-				SummaryView = SingletonViewFactoryInstance.ConcreteGUIFailureSummaryView(GUIGamePlayView);        // działa, jeśli ViewManager dziedziczy z SingletonViewFactory
+				SummaryView = FactoryInstance.ConcreteGUIFailureSummaryView(GUIGamePlayView);                 
 				((GUIFailureSummaryView)SummaryView).transform.SetParent(FindObjectOfType<ViewManager>().transform);
 				break;
 		}
