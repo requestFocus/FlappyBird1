0a167e24acd277637d30c24d96c9d0b8d680ba99
requestFocus <thezodiac@wp.pl>
2018-05-08 19:23:09 +0200

    where TFactory : MonoBehaviour

--- Flappy Bird Game/Assets/Scripts/Game/Singleton.cs
+++ Flappy Bird Game/Assets/Scripts/Game/Singleton.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton<TFactory> : MonoBehaviour
+public class Singleton<TFactory> : MonoBehaviour where TFactory : MonoBehaviour
 {
 	private static TFactory _factoryInstance;
 
@@ -13,15 +13,11 @@ public class Singleton<TFactory> : MonoBehaviour
 		{
 			if (_factoryInstance == null)
 			{
-				//GameObject singleton = new GameObject(typeof(TFactory) + "SingletonGeneric");
-				//_factoryInstance = singleton.AddComponent<TFactory>();						  // nie można dodać komponentu w ten sposób
-
-				_factoryInstance = (TFactory)Activator.CreateInstance(typeof(TFactory));        // WARNING: createinstance wykonane, ale "You are trying to create a MonoBehaviour using the 'new' keyword"
+				GameObject singleton = new GameObject(typeof(TFactory) + "SingletonGeneric");
+				_factoryInstance = singleton.AddComponent<TFactory>();                        // nie można dodać komponentu w ten sposób
 			}
 			return _factoryInstance;
 		}
 	}
 }
 
-
-// jak stworzyc dziedziczący po monobehaviour obiekt singletona na podstawie typu generycznego?
