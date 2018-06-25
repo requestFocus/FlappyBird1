afe119a838a75e572a53898eff29191d6e8d10a8
requestFocus <thezodiac@wp.pl>
2018-06-25 22:51:12 +0200

    AchievementsView in progress

--- Flappy Bird Game/Assets/Scripts/Menu/PlayerStats/MultiplePlayerStatsView.cs
+++ Flappy Bird Game/Assets/Scripts/Menu/PlayerStats/MultiplePlayerStatsView.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using Zenject;
 
 public class MultiplePlayerStatsView : MonoBehaviour
@@ -10,20 +10,46 @@ public class MultiplePlayerStatsView : MonoBehaviour
 	[Inject]
 	private SinglePlayerStatsView _singlePlayerStatsView;
 
+	private List<SinglePlayerStatsView> _listOfSingleEntries = new List<SinglePlayerStatsView>();
+
+	private void Update()
+	{
+		FollowMouse();
+	}
+
 	public void ListPlayerWithStats(ProjectData projectData, Vector3 playerNamePos, Vector3 highscorePos, Vector3 achievementsPos)
 	{
-		for (int i = 0; i < projectData.EntireList.Count; i++)
-		{
-			SinglePlayerStatsView singlePlayerStatsViewInstance = Instantiate(_singlePlayerStatsView);
-			_container.Inject(singlePlayerStatsViewInstance);
-			singlePlayerStatsViewInstance.transform.SetParent(gameObject.transform);
-			singlePlayerStatsViewInstance.name = "Entry" + i;
+		FillTheListOfSingleEntries(projectData);
 
-			singlePlayerStatsViewInstance.CreateSinglePlayerStatsView(projectData.EntireList[i], playerNamePos, highscorePos, achievementsPos);
+		for (int i = 0; i < 4; i++)
+		{
+			_listOfSingleEntries[i].CreateSinglePlayerStatsView(projectData.EntireList[i], playerNamePos, highscorePos, achievementsPos);
 
 			playerNamePos.y -= 30;
 			highscorePos.y -= 30;
 			achievementsPos.y -= 30;
 		}
 	}
+
+	private void FillTheListOfSingleEntries(ProjectData projectData)
+	{
+		for (int i = 0; i < projectData.EntireList.Count; i++)
+		{
+			SinglePlayerStatsView singlePlayerStatsViewInstance = Instantiate(_singlePlayerStatsView);
+			_container.Inject(singlePlayerStatsViewInstance);
+			singlePlayerStatsViewInstance.transform.SetParent(gameObject.transform);
+			_listOfSingleEntries.Add(singlePlayerStatsViewInstance);
+		}
+	}
+
+	private void FollowMouse()
+	{
+		if (Input.GetMouseButton(0) && Input.mousePosition.x > 260 && Input.mousePosition.x < 650 && Input.mousePosition.y < 350 && Input.mousePosition.y > 250)
+			Debug.Log(Input.mousePosition.y);
+	}
 }
+
+// lewy gorny 260, 350
+// prawy gorny 655, 355
+// lewy dolny 260, 250
+// prawy dolny 655, 255
